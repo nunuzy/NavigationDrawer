0e4990442d12f6881b633090a6f16d7109b93b05
nunuzy <fnukhi@gmail.com>
2020-11-24 10:51:31 +0700

    first commit

---
+++ app/src/main/java/com/nunuzy/mynavigationdrawer/SubwayActivity.kt
@@ -0,0 +1,12 @@
+package com.nunuzy.mynavigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubwayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
